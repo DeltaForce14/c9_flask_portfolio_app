cc88a4551d5d3f963ada4f40691103ff08a85356
Dita Peskova <33196424+DeltaForce14@users.noreply.github.com>
2019-06-07 18:44:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "You are awesome!"
+	new_name = str(name) + " You are awesome!"
 	return render_template('index.html', name=new_name)
 
 
